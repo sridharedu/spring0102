3a2548020b775e3039280bbb44d63de2f5675978
sridhar nandipati <sridhar4edu@gmail.com>
2024-02-01 12:16:44 +0530

    Initial commit

---
+++ src/main/java/com/learn/springboot/spring0102/Spring0102Application.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.spring0102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springboot/spring0102/Spring0102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.spring0102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
